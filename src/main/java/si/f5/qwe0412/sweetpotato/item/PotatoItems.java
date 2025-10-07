3a16153c603af3971e8b70cb33479028da1a317d
qwe0412 <qwe0412.mc@gmail.com>
2025-10-07 17:16:39 +0900

    created project

---
+++ src/main/java/si/f5/qwe0412/sweetpotato/item/PotatoItems.java
@@ -0,0 +1,19 @@
+package si.f5.qwe0412.sweetpotato.item;
+
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import si.f5.qwe0412.sweetpotato.Sweetpotato;
+
+public class PotatoItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Sweetpotato.MODID);
+
+    public static final DeferredItem<Item> SWEET_POTATO = ITEMS.register("sweet_potato",() -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(3).saturationModifier(0.3f).build())));
+    public static final DeferredItem<Item> BAKED_SWEET_POTATO = ITEMS.register("baked_sweet_potato",() -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(7).saturationModifier(0.6f).build())));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
